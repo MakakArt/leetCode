9ee396eb7b7ea8e9a215d618e93edd12515d86a4
Arthur Zhagiparov <64406649+MakakArt@users.noreply.github.com>
2025-12-02 03:28:05 +0500

    Time: 0 ms (100.00%) | Memory: 4.1 MB (76.92%) - LeetSync

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.go
@@ -0,0 +1,8 @@
+func generateTheString(n int) string {
+    str := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    if n%2 == 0 {
+        return str[:n-1]+"b"
+    } else {
+        return str[:n]
+    }
+}
