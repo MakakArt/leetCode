4c8b233f73a7b4362ecf07ef2438e4598a27aaed
Arthur Zhagiparov <64406649+MakakArt@users.noreply.github.com>
2026-03-27 02:21:42 +0500

    Time: 11 ms (5.62%) | Memory: 8.2 MB (30.62%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -0,0 +1,24 @@
+func setZeroes(matrix [][]int)  {
+    var row bool
+    columns := make([]bool, len(matrix[0]))
+    for i := 0; i < len(matrix); i++ {
+        for j := 0; j < len(matrix[i]); j++ {
+            if matrix[i][j] == 0 {
+                row = true
+                columns[j] = true
+            }
+            if columns[j] { 
+                matrix[i][j] = 0
+                k := i
+                for k > -1 {
+                    matrix[k][j] = 0
+                    k--
+                }
+            }
+        }
+        if row {
+            matrix[i] = make([]int, len(matrix[i]))
+        }
+        row = false
+    }
+}
