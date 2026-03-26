459fcba3a1e45d1309f7584f16069530e6c077e0
Arthur Zhagiparov <64406649+MakakArt@users.noreply.github.com>
2026-03-27 02:44:17 +0500

    Time: 12 ms (5.62%) | Memory: 8.6 MB (11.05%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.go
+++ 73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -1,7 +1,6 @@
 func setZeroes(matrix [][]int)  {
     var row bool
     columns := make([]bool, len(matrix[0]))
-    empty := make([]int, len(matrix[0]))
     for i := 0; i < len(matrix); i++ {
         for j := 0; j < len(matrix[i]); j++ {
             if matrix[i][j] == 0 {
@@ -12,13 +11,15 @@ func setZeroes(matrix [][]int)  {
                     matrix[k][j] = 0
                     k--
                 }
+                k = j
+                for k > -1 {
+                    matrix[i][k] = 0
+                    k--
+                }
             }
-            if columns[j] { 
+            if columns[j] || row { 
                 matrix[i][j] = 0
             }
-        }
-        if row {
-            matrix[i] = empty
         }
         row = false
     }
