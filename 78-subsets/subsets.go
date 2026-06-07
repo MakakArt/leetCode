0fe669a81ea77feddde99efedce55b4a1b2faf3e
Arthur Zhagiparov <64406649+MakakArt@users.noreply.github.com>
2026-06-07 20:29:04 +0500

    Time: 0 ms (100.00%) | Memory: 4.2 MB (81.67%) - LeetSync

---
+++ 78-subsets/subsets.go
@@ -0,0 +1,19 @@
+func subsets(nums []int) [][]int {
+    var result [][]int
+    buff := make([]int, len(nums))
+    var r func(depth, curr int)
+    r = func(depth, curr int) {
+        res := make([]int, curr)
+        copy(res, buff)
+        result = append(result, res)
+        if depth > len(nums) {
+            return
+        }
+        for i := depth; i < len(nums); i++ {
+            buff[curr] = nums[i]
+            r(i+1, curr+1)
+        }
+    }
+    r(0, 0)
+    return result
+}
