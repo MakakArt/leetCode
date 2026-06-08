3a941a8d3724fa19459397a4e1035d6551caf628
Arthur Zhagiparov <64406649+MakakArt@users.noreply.github.com>
2026-06-09 02:30:11 +0500

    Time: 101 ms (72.71%) | Memory: 4.1 MB (79.20%) - LeetSync

--- 79-word-search/word-search.go
+++ 79-word-search/word-search.go
@@ -1,8 +1,9 @@
 func exist(board [][]byte, word string) bool {
     n := len(board) - 1
     m := len(board[0]) - 1
-    var r func(s, x, y int, been []bool) bool
-    r = func(s, x, y int, been []bool) bool {
+    been := make([]bool, encode(n, m, n + 1) + 1)
+    var r func(s, x, y int) bool
+    r = func(s, x, y int) bool {
         if s == len(word) {
             return true
         }
@@ -12,7 +13,8 @@ func exist(board [][]byte, word string) bool {
         if word[s] == board[x][y] {
             been[encode(x, y, n+1)] = true
             s++
-            if r(s, x+1, y, been) || r(s, x, y-1, been) || r(s, x-1, y, been) || r(s, x, y+1, been) {
+            if r(s, x+1, y) || r(s, x, y-1) || r(s, x-1, y) || r(s, x, y+1) {
+                been[encode(x, y, n+1)] = false
                 return true
             }
             been[encode(x, y, n+1)] = false
@@ -22,8 +24,7 @@ func exist(board [][]byte, word string) bool {
     for i := 0; i < len(board); i++ {
         for j := 0; j < len(board[0]); j++ {
             if board[i][j] == word[0] {
-                been := make([]bool, encode(n, m, n + 1) + 1)
-                if r(0, i, j, been) { 
+                if r(0, i, j) { 
                     return true
                 }
             }
@@ -36,6 +37,3 @@ func encode(x, y, w int) int {
     return y * w + x
 }
 
-// func decode(code, w int) (int, int) {
-//     return code % w, code / w
-// }
