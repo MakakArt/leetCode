76b4952680edbf9d78e4bf193973a8af7d812384
Arthur Zhagiparov <64406649+MakakArt@users.noreply.github.com>
2026-06-11 21:00:51 +0500

    Time: 5 ms (82.54%) | Memory: 7.3 MB (44.16%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.go
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.go
@@ -1,18 +1,10 @@
-func removeDuplicates(nums []int) int {    
-    count := 1
-    for i := 1; i < len(nums); i++ {
-        if nums[i] != nums[i-1] {
-            if count > 2 {
-                nums = append(nums[:i-count+2], nums[i:]...)
-                i = i-count+1
-            }
-            count = 1
-        } else {
-            count++
+func removeDuplicates(nums []int) int {
+    k := 0
+    for _, x := range nums {
+        if k < 2 || nums[k-2] != x {
+            nums[k] = x
+            k++
         }
     }
-    if count > 2 {
-        nums = nums[:len(nums)-(count-2)]
-    }
-    return len(nums)
+    return k
 }
